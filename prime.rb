d10d64709d46305f08e7d5e7dbc5a784adb7beff
Chibuisi Enyia <enyia21@gmail.com>
2020-04-20 08:36:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,27 @@
 # Add  code here!
-# def prime(number)
-#   lo = 2
-#   hi = Integer.sqrt(number.abs)
-#   prime_test_array = (2..number).to_a
-#   prime_test_array
-#   if number < 0
-#       if number == -1
-#         return false
-#       else
-#   end
-# end
+def prime(number)
+  lo = 2
+  hi = Integer.sqrt(number.abs)
+  prime_test_array = (2..number).to_a
+  prime_test_array << prime_test_array.select{|number| number.odd?}
+# test multiple numbers in an array to find out if they are prime_test_array
+# eliminate even numbers not equal to 2 because they are not prime
+if number > 3 || number < -3 
+  if number.even?
+    return false
+  else
+      if prime_test_array.any?{|dividend| number%dividend == 0} 
+        return false
+      else
+        return true
+      end
+    end
+  end
+end
+  # prime_test_array.any?
 number = 1012
-number = Integer.sqrt(number.abs)
-array = []
-array = (2..number).to_a
-puts array
+# number = Integer.sqrt(number.abs)
+# array = []
+# array = (2..number).to_a
+# puts array
+puts prime(number)
