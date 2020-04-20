2c8818cb2d8b97d4050ba49bf3ac9238456e97e8
Chibuisi Enyia <enyia21@gmail.com>
2020-04-20 06:39:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+# def prime(number)
+#   lo = 2
+#   hi = Integer.sqrt(number.abs)
+#   prime_test_array = 2...hi
+#
+#   if number < 0
+#       if number == -1
+#         return false
+#       else
+#   end
+# end
+number = 1012
+number = Integer.sqrt(number.abs)
+array = []
+array = (2..number).to_a
+puts array
